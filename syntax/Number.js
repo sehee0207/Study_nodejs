d761e2fbce46abe4d5af022582a6ad00eed29bc0
sehee <lolry0207@gmail.com>
2021-02-03 13:15:15 +0900

    Initial commit

---
+++ syntax/Number.js
@@ -0,0 +1,5 @@
+console.log(4*3);
+
+// node syntax/Number.js
+// node 야 syntax 파일의 Number.js 를 실행해줘!
+// Window 에서 안 된다면 syntax\Number.js 역슬래쉬 사용
---
+++ syntax/String.js
@@ -0,0 +1,4 @@
+console.log('1'+'1');
+// 문자열 + 문자열의 의미 이므로 11이 나옴
+
+console.log('dfjakdlfj;dfjdklf;jdlk;fjdlak;fjdkfadfhak'.length);
